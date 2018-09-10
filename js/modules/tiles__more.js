251c02116aa03638b5dc3b4d664456c80cad83a8
adi <adityabhat@lazaro.in>
2018-09-10 14:55:17 +0530

    Bug : Play Video on Hover : Video breaks when a pause occurred before the play
    The video's play method is no longer a synchronous method.
    It now returns a promise.
    Things break when the pause method is invoked before the play one resolved.

--- js/modules/tiles__more.js
+++ js/modules/tiles__more.js
@@ -34,11 +34,32 @@ onceVariableIsSet( "__SETTINGS.client.baseExperience" ).then( function ( baseExp
 		$videos.addClass( "done" );
 	}
 
+	// This var holds the return value (if any) of the call to the `video.play()`.
+	var videoSetToPlay;
+
 	$( ".js_tile" ).on( "mouseenter", function ( event ) {
 
 		var $tile = $( event.target ).closest( ".js_tile" );
 		var $video = $tile.find( "video" );
-		$video.get( 0 ).play();
+
+		// Depending on whether the play function returns a promise or not,
+		// 	we have to deal with the situation accordingly
+			// Only play the video if a playback hasn't already been scheduled
+		if ( ! videoSetToPlay )
+			videoSetToPlay = $video.get( 0 ).play();
+			// If a promise was returned
+		if ( videoSetToPlay && videoSetToPlay.then ) {
+			videoSetToPlay.then( function () {
+				videoSetToPlay = null;
+			} )
+			videoSetToPlay.catch( function () {
+				videoSetToPlay = null;
+			} )
+		}
+		else {
+			videoSetToPlay = null;
+		}
+
 		$video.addClass( "done" );
 
 	} );
@@ -46,8 +67,21 @@ onceVariableIsSet( "__SETTINGS.client.baseExperience" ).then( function ( baseExp
 	$( ".js_tile" ).on( "mouseleave", function ( event ) {
 
 		var $tile = $( event.target ).closest( ".js_tile" );
+
 		var $video = $tile.find( "video" );
-		$video.get( 0 ).pause();
+		// If a promise was returned when the video was scheduled to play, then schedule a pause after that
+		if ( videoSetToPlay && videoSetToPlay.then ) {
+			videoSetToPlay.then( function () {
+				$video.get( 0 ).pause();
+			} )
+			videoSetToPlay.catch( function () {
+				$video.get( 0 ).pause();
+			} )
+		}
+		else {
+			$video.get( 0 ).pause();
+		}
+
 		$video.removeClass( "done" );
 
 	} );
