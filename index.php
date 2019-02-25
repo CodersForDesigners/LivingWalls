84702cda519168d2c75b5e6f6a12025223fb0c3d
aditya <nostalgicdodo@users.noreply.github.com>
2019-02-25 16:58:56 +0530

    SEO : Change the page title to include the word "OFFICIAL"
    This is to distinguish our search result from the hordes of imitators.

--- index.php
+++ index.php
@@ -41,7 +41,7 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
 	<!-- Page Title | Page Name -->
-	<title>The Secretly Kickass 4BHK House | Secret Soil | LivingWalls</title>
+	<title>OFFICIAL LivingWalls | The Secretly Kickass 4BHK House | Secret Soil</title>
 
 	<?php echo gethead(); ?>
 
